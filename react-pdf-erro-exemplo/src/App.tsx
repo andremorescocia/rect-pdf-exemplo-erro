3757a3056f10f47e9a66d6b02c0f856d8fb5992c
Fabio Tessaro <fabio@ciasistemas.com.br>
2024-12-05 13:27:33 -0300

    Comit 6

--- react-pdf-erro-exemplo/src/App.tsx
+++ react-pdf-erro-exemplo/src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import pdfData from './PdfData.json'
 import PDF from './PDF';
 import GeneratorPdf from './GeneratorPdf';
 
 const App: React.FC = () => {
+  const [currentState, setCurrentState] = useState("Hello!")
+
   async function handleGeneratePdf(): Promise<void> {
-
     const dados: any = pdfData;
-
     console.log('dados do json', dados);
+    setCurrentState('Generating Pdf... (about 3 minutes)');
 
     const relatorio = await PDF({
       documento: (
@@ -18,13 +19,14 @@ const App: React.FC = () => {
     });
 
     relatorio.AbrirArquivo();
+    setCurrentState("Pdf finished!")
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Hello, TypeScript!</h2>
-        <button onClick={async () => {await handleGeneratePdf()}}>Click here to generate PDF</button>
+        <h2>{currentState}</h2>
+        <button onClick={() => {handleGeneratePdf()}}>Click here to generate PDF</button>
       </header>
     </div>
   );
